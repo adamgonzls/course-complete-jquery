77c1d02b5358ff3fe51b52d280e2380a725660ac
Adam Gonzales <adamgonzales1@gmail.com>
2016-11-28 07:35:59 -0700

    exercises

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
-	$("#output").text("I added this with jQuery");
+$(document).ready(function() {
+
+	$("#myForm").submit(function(e) {
+		e.preventDefault();
+		console.log(e);
+		$("#mybox").text("it was submitted");
+	});
+
+	// $("select").change(function(e) {
+	// 	console.log(e.target.value);
+	// });
+
+	$("select").on("change", function(e) {
+		console.log(e.target.value);
+	});
+
 });
