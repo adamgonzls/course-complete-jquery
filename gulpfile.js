afc03000b7e71f5cea1b03561316f0335a924b9e
Adam Gonzales <adamgonzales1@gmail.com>
2016-12-04 20:08:21 -0700

    add support for php server

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 var gulp 					= require('gulp');
+var php						= require('gulp-connect-php');
 var browserSync 	= require('browser-sync').create();
 var sass 					= require('gulp-sass');
 var autoprefixer 	= require('gulp-autoprefixer');
 
 // static server + watch scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['php','sass'], function() {
 	browserSync.init({
-		server: "./"
+		server: "./",
+		socket: {
+			namespace: "localhost:3000"
+		}
 	});
 
 	gulp.watch("./sass/*.scss", ['sass']);
@@ -14,8 +18,15 @@ gulp.task('serve', ['sass'], function() {
 	gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
-// compile sass & auto-inject into browsers
+gulp.task('php', function() {
+	php.server({ 
+		base: './', 
+		hostname: 'localhost',
+		port: 3000, 
+		keepalive: true})
+})
 
+// compile sass & auto-inject into browsers
 //sass
 gulp.task('sass', function() {
 	return gulp.src('./sass/*.scss')
